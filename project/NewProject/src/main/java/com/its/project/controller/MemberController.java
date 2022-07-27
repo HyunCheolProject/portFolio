2c86736f1e3260c2130b5c01dfb62355c5799585
HyunCheolProject <loveyou160@naver.com>
2022-07-27 10:04:09 +0900

    member 로그인 성공

--- project/NewProject/src/main/java/com/its/project/controller/MemberController.java
+++ project/NewProject/src/main/java/com/its/project/controller/MemberController.java
@@ -4,10 +4,13 @@ import com.its.project.dto.MemberDTO;
 import com.its.project.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/member")
@@ -28,7 +31,24 @@ public class MemberController {
         return "member/login";
     }
 
+    // 로그인 화면 요청
+    @GetMapping("/login-form")
+    public String loginForm() {
+        return "member/login";
+    }
 
-
+    // 로그인 처리
+    @PostMapping("/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session, Model model) {
+        MemberDTO member = memberService.login(memberDTO);
+        if (member != null) {
+            session.setAttribute("id", member.getId());
+            session.setAttribute("memberId", member.getMemberId());
+            model.addAttribute("member", member);
+            return "member/mypage";
+        } else {
+            return "member/login";
+        }
+    }
 
 }
---
+++ project/NewProject/src/main/webapp/WEB-INF/views/member/login.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: khc_9
+  Date: 2022-07-27
+  Time: 오전 10:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>login.jsp</title>
+</head>
+<body>
+    <h2>로그인</h2>
+    <form action="/member/login" method="post">
+        아이디: <input type="text" name="memberId"><br>
+        비밀번호: <input type="password" name="memberPw"><br>
+        <input type="submit" value="로그인">
+    </form>
+</body>
+</html>
