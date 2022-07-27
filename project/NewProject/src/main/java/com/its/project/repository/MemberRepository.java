7d4ffc3a399555f94782c4faa513c2696b0de45a
HyunCheolProject <loveyou160@naver.com>
2022-07-27 09:30:32 +0900

    member 세팅

---
+++ project/NewProject/src/main/java/com/its/project/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package com.its.project.repository;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
+
+
+
+
+
+
+
+
+
+
+}
---
+++ project/NewProject/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: khc_9
+  Date: 2022-07-27
+  Time: 오전 9:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>index.jsp</title>
+</head>
+<body>
+
+</body>
+</html>
